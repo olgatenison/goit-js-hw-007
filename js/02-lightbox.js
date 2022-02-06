485d2ec346f0916714a902e54a41ea3635a115dd
olgatenison <88651744+olgatenison@users.noreply.github.com>
2022-02-06 18:48:34 +0200

    update 2 gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,12 +6,12 @@ console.log(galleryItems);
 
 const markup = galleryItems.map (({preview, original, description}) => {
     return `
-    <a class="gallery__item" 
+    <li><a class="gallery__item" 
         href="${original}">
         <img class="gallery__image" 
         src="${preview}" 
         alt="${description}/>
-    </a>`
+    </a></li>`
 }) .join("");
 console.log(markup);
 

e82e7567b336fa7f10fb01e96cbd5fe67362c005
olgatenison <88651744+olgatenison@users.noreply.github.com>
2022-02-06 16:22:31 +0200

    add second gallery items

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// библиотека SimpleLightbox
+//
 
 console.log(galleryItems);
+
+const markup = galleryItems.map (({preview, original, description}) => {
+    return `
+    <a class="gallery__item" 
+        href="${original}">
+        <img class="gallery__image" 
+        src="${preview}" 
+        alt="${description}/>
+    </a>`
+}) .join("");
+console.log(markup);
+
+const gallaryList = document.querySelector('.gallery');
+gallaryList.insertAdjacentHTML('beforeend', markup);
+
+// let gallery = new SimpleLightbox('.gallery a', 
+// { captionsData: '${description}'
+// captionDelay: 250
+// });
+
+
+
+    

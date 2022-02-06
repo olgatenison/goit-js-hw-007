a8a3376223bf9c3c7b3ad327a1d6483199f99e26
olgatenison <88651744+olgatenison@users.noreply.github.com>
2022-02-06 13:53:56 +0200

    render first gallery items

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+
+const markup = galleryItems.map (({preview, original, description}) => {
+     return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>`
+}) .join("");
+console.log(markup);
+
+const gallaryList = document.querySelector('.gallery');
+gallaryList.insertAdjacentHTML('beforeend', markup);
+
+
+
+

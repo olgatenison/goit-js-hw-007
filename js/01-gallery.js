20cc5ce6fd6ad92a1f60b2466498b4a82c2499af
olgatenison <88651744+olgatenison@users.noreply.github.com>
2022-02-06 14:56:12 +0200

    open and close img of first gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,9 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-console.log(galleryItems);
+// подключение скрипта и стилей библиотеки модального окна basicLightbox
+// Создание и рендер разметки по массиву данных galleryItems 
+// и предоставленному шаблону элемента галереи.
 
+console.log(galleryItems);
 
 const markup = galleryItems.map (({preview, original, description}) => {
      return `
@@ -20,6 +22,33 @@ console.log(markup);
 
 const gallaryList = document.querySelector('.gallery');
 gallaryList.insertAdjacentHTML('beforeend', markup);
+
+//Открытие модального окна по клику на элементе галереи
+
+gallaryList.addEventListener ('click', onGalleryItemClick )
+
+function onGalleryItemClick(evt){
+
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== "IMG") {
+    return;
+    }
+    console.log(evt.target)
+
+    const selectedImg = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}">`)
+    selectedImg.show()
+
+    // закрытие модального окна по нажатию клавиши Escape
+
+    window.addEventListener('keydown', onOpenGalleryItemKeydown)
+    function onOpenGalleryItemKeydown (evt){
+    if (evt.code == 'Escape') {
+        selectedImg.close() 
+        } 
+    }       
+}
 
 
 
